c5c990000044bd309eec61b22f4d658dd111c4d5
LunarMelody <103903444+LunarMelody@users.noreply.github.com>
2023-11-26 22:45:56 +0400

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw Error("Root not found!");
+}
+
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
