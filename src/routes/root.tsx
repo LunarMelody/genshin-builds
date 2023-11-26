211bce1ccc591cc67d968e5a410916bf03cc0562
LunarMelody <103903444+LunarMelody@users.noreply.github.com>
2023-11-26 23:22:27 +0400

    feat: added header to root layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "@/components/header.tsx";
 
 export const Root: FC = () => {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 };
