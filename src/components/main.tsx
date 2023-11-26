1601937348c411da10eb89c7e15a967cd889f92b
LunarMelody <103903444+LunarMelody@users.noreply.github.com>
2023-11-26 23:22:11 +0400

    feat: created main component

---
+++ src/components/main.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from "react";
+import { cn } from "@/utils/cn.ts";
+
+interface MainProps {
+  className?: string;
+  children?: ReactNode | undefined;
+}
+
+export const Main: FC<MainProps> = ({ children, className }) => {
+  const classes = cn(className, "grid-area-[main]");
+
+  return <main className={classes}>{children}</main>;
+};
