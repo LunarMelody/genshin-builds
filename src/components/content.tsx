852c81624f6cf47ac9dd5fb51f2a5b0cccd8a88d
LunarMelody <103903444+LunarMelody@users.noreply.github.com>
2023-11-26 23:31:41 +0400

    feat: main content wrapper will have smaller padding on mobile

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -7,7 +7,7 @@ interface ContentProps {
 }
 
 export const Content: FC<ContentProps> = ({ children, className }) => {
-  const classes = cn(className, "flex flex-col p-4 mx-auto max-w-[1300px] w-full");
+  const classes = cn(className, "flex flex-col px-2 py-2 md:p-4 mx-auto max-w-[1300px] w-full");
 
   return <div className={classes}>{children}</div>;
 };
